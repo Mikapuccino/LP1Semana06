a9569cf41c126beadec71ad3a64a8eccdd9f216c
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-04-19 17:12:57 +0100

    Fix main program, prograsm works properly

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -25,18 +25,18 @@ namespace GameSix
 
             Foes[0].PickupPowerUp(PowerUp.Shield, 10);
             Console.WriteLine("Foe 1 gained 10 shield.");
-            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth + " health.");
-            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield + " shield.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth() + " health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield() + " shield.");
 
             Foes[0].TakeDamage(15);
             Console.WriteLine("Foe 1 took 15 damage, losing 5 health.");
-            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth + " health.");
-            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield + " shield.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth() + " health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield() + " shield.");
 
             Foes[0].PickupPowerUp(PowerUp.Health, 2);
             Console.WriteLine("Foe 1 gained 2 health.");
-            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth + " health.");
-            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield + " shield.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth() + " health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield() + " shield.");
         }
     }
 }
