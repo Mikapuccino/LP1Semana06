71a27ee0abf2c3b1fc57e44877de655afb59fc31
Nelson <nelsonfcmilheiro@gmail.com>
2023-04-21 13:06:50 +0100

    Add GameSixFriday to solution

---
+++ GameSixFriday/Foe.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameSix
+{
+    public class Foe
+    {
+        private string name;
+        private float health;
+        private float shield;
+
+        static int foePowerUp;
+        
+        // Foe constructor, defines name, health and shield
+        public Foe()
+        {
+            this.name = SetName();
+            health = 100;
+            shield = 0;
+        }
+
+        static Foe()
+        {
+            foePowerUp = 0;
+        }
+
+        // Returns the name of foe
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Returns the current health of foe
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        // Returns the current shield of foe
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public static int GetPowerUp()
+        {
+            return foePowerUp;
+        }
+
+        // Sets the name of foe to what user input
+        public string SetName()
+        {
+            Console.WriteLine("Choose the name of your foe: ");
+            string newName = Console.ReadLine();
+            newName = newName.Trim();
+            return newName;
+        }
+        
+        // Reduces the Foe's health by the damage not blocked by shield
+        public void TakeDamage(float damage)
+        {
+            // If shield isn't reduced to 0, Foe takes no damage
+            shield -= damage;
+            if (shield < 0)
+            {
+                // Health is reduced by damage not blocked by shield
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+        // Increases health or shield depending on powerup that was picked up
+        public void PickupPowerUp(PowerUp power, float amount)
+        {
+            if (power == PowerUp.Health)
+            {
+                health += amount;
+
+                // If health exceeds 100, sets health to 100
+                if (health > 100)
+                {
+                    health = 100;
+                }
+            }
+
+            if (power == PowerUp.Shield)
+            {
+                shield += amount;
+
+                // If shield exceeds 100, sets shield to 100
+                if (shield > 100)
+                {
+                    shield = 100;
+                }
+            }
+
+            foePowerUp++;
+        }
+    }
+}
---
+++ GameSixFriday/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GameSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many enemies will be created?");
+            int nOfFoes = int.Parse(Console.ReadLine());
+
+            var Foes = new Foe[nOfFoes];
+
+            for (int i = 0; i < nOfFoes; i++)
+            {
+                Foe enemy = new Foe();
+                Foes[i] = enemy;
+            }
+
+            for (int i = 0; i < nOfFoes; i++)
+            {
+                Console.WriteLine($"Foe " + (i + 1) + " is called " +
+                Foes[i].GetName() + ".");
+            }
+
+            Foes[0].PickupPowerUp(PowerUp.Shield, 10);
+            Console.WriteLine("Foe 1 gained 10 shield.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth() + " health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield() + " shield.");
+
+            Foes[0].TakeDamage(15);
+            Console.WriteLine("Foe 1 took 15 damage, losing 5 health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth() + " health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield() + " shield.");
+
+            Foes[0].PickupPowerUp(PowerUp.Health, 2);
+            Console.WriteLine("Foe 1 gained 2 health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetHealth() + " health.");
+            Console.WriteLine($"Foe 1 has " + Foes[0].GetShield() + " shield.");
+
+            // Should be 2 powerups
+            Console.WriteLine($"Foe 1 got " + Foe.GetPowerUp() +
+            " powerups.");
+
+            Console.WriteLine("Thank you for using this program!");
+        }
+    }
+}
