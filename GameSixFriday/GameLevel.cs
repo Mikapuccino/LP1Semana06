f0a297047da8cd51293231b0d4a925365647313f
Nelson <nelsonfcmilheiro@gmail.com>
2023-04-21 13:26:21 +0100

    Add getter methods for number of rooms, foes and difficulty to GameLevel

--- GameSixFriday/GameLevel.cs
+++ GameSixFriday/GameLevel.cs
@@ -7,13 +7,35 @@ namespace GameSixFriday
 {
     public class GameLevel
     {
-        private int rooms;
+        private Foe[] rooms;
+        private int foes;
         private Difficulty difficulty;
         
         public GameLevel(int rooms, Difficulty difficulty)
         {
-            this.rooms = rooms;
+            this.rooms = new Foe[rooms];
             this.difficulty = difficulty;
+            foes = 0;
+        }
+
+        public Difficulty GetDifficulty()
+        {
+            return difficulty;
+        }
+
+        public int GetRooms()
+        {
+            return rooms.Length;
+        }
+
+        public int GetFoes()
+        {
+            return foes;
+        }
+        
+        public void SetFoeInRoom()
+        {
+
         }
     }
 }
